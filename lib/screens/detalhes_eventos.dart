ee81f012c39d052b66d1bb4ad209dcd167f8a46f
LenaMenezes <helena.mecedo5@etec.sp.gov.br>
2024-06-08 16:46:08 -0300

    criação da tela de detalhes do eventos

---
+++ lib/screens/detalhes_eventos.dart
@@ -0,0 +1,214 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:geek_connect/models/eventos.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:geek_connect/widgets/constantes.dart';
+
+
+class DetalhesScreen extends StatefulWidget {
+  final Evento eventos;
+  const DetalhesScreen({super.key, required this.eventos});
+
+  @override
+  State<DetalhesScreen> createState() => _DetalhesScreenState();
+}
+
+class _DetalhesScreenState extends State<DetalhesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      bottomNavigationBar: Container(
+        padding: const EdgeInsets.all(10),
+        child: Row(
+          children: [
+            Expanded(
+              flex: 6,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: kprimaryColor,
+                  foregroundColor: Colors.white,
+                ),
+                child: const Text(btnComprar),
+              ),
+            ),
+            const SizedBox(width: 10,),
+            Expanded(
+              child: IconButton(
+                onPressed: () {},
+                style: IconButton.styleFrom(
+                  shape: CircleBorder(
+                    side: BorderSide(
+                      color: Colors.grey.shade300,
+                      width: 2,
+                    ),
+                  )
+                ),
+                icon: Icon(
+                  widget.eventos.favorito ? Iconsax.heart5 : Iconsax.heart,
+                  color: widget.eventos.favorito ? Colors.red : Colors.black,
+                  size: 20,),
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Positioned(
+                  child: Container(
+                    height: MediaQuery.of(context).size.width - 200,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(widget.eventos.imgEvento),
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 20,
+                  left: 10,
+                  right: 10,
+                  child: Row(
+                    children: [
+                      IconButton(
+                        onPressed: () => Navigator.pop(context),
+                        style: IconButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                            fixedSize: const Size(50, 50)),
+                        icon: const Icon(CupertinoIcons.chevron_back),
+                      ),
+                      const Spacer(),
+                      IconButton(
+                        onPressed: () {},
+                        style: IconButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                            fixedSize: const Size(50, 50)),
+                        icon: const Icon(Iconsax.notification),
+                      ),
+                    ],
+                  ),
+                ),
+                Positioned(
+                  left: 0,
+                  right: 0,
+                  top: MediaQuery.of(context).size.width - 220,
+                  child: Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.all(20),
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(20),
+                        topLeft: Radius.circular(20),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Center(
+              child: Container(
+                width: 50,
+                height: 5,
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade300,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+            ),  
+            SizedBox(height: 10),      
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    widget.eventos.eventoName,
+                    style: const TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      const Icon(
+                        Iconsax.calendar,
+                        size: 18,
+                        color: Colors.grey,
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        widget.eventos.eventoData,
+                        style: const TextStyle(
+                          fontSize: 15,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      const Icon(
+                        Iconsax.clock,
+                        size: 18,
+                        color: Colors.grey,
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        widget.eventos.horario,
+                        style: const TextStyle(
+                          fontSize: 15,
+                          color: Colors.grey,
+                        ),
+                      ),
+                     
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    widget.eventos.eventoDescri,
+                    textAlign: TextAlign.justify,
+                    style: const TextStyle(
+                      fontSize: 15,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Text(
+                    widget.eventos.eventoLocal,
+                    textAlign: TextAlign.justify,
+                    style: const TextStyle(
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
